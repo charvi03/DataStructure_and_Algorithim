2668d211c793607c8ec354d86ee037e8cf213a64
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-09-03 11:50:35 +0530

    init

--- src/MathsBasics/divisors.java
+++ src/MathsBasics/divisors.java
@@ -1,4 +1,39 @@
 package MathsBasics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 //Exactly 3 divisors
 public class divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        With3Divisors(n);
+    }
+
+    static void With3Divisors(int n) {
+        int count = 0;
+        boolean[] prime = new boolean[n + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        // all prime no. upto given number
+        for (int i = 2; i * i <= n; i++) {
+            for (int j = 2 * i; j <= n; j = j + i) {
+                prime[j] = false;
+            }
+        }
+
+        System.out.println("Numbers with 3 divisors : ");
+        for (int i = 0; i * i <= n; i++) {
+            if (prime[i]) {
+                System.out.print(i * i + " ");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.print("total numbers " + count);
+    }
+
+
 }
--- src/MathsBasics/primalaity_test.java
+++
@@ -1,4 +0,0 @@
-package MathsBasics;
-//primality test
-public class primalaity_test {
-}
