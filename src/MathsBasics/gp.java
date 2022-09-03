dff1e1b376a4cde7cba9a7c8f888fac89716633a
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-09-03 11:06:18 +0530

    init

--- src/MathsBasics/gp.java
+++ src/MathsBasics/gp.java
@@ -1,4 +1,23 @@
 package MathsBasics;
+
+import java.util.Scanner;
+
 //gp term
 public class gp {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter 1st term");
+        int a= sc.nextInt();
+        System.out.println("Enter 2nd term");
+        int b= sc.nextInt();
+        System.out.println("Enter total number of terms");
+        int n=sc.nextInt();
+        System.out.println(gpTerm(a,b,n));
+
+    }
+    static double gpTerm(int a,int b , int n){
+        int r= b/a;
+       double last=a*(Math.pow(r,(n-1)));
+       return last;
+    }
 }
