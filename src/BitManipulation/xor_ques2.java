0db3cec09f75b252fdc139ab510cf028f8e56d9b
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-09-06 11:06:14 +0530

    init

--- src/BitManipulation/xor_ques2.java
+++ src/BitManipulation/xor_ques2.java
@@ -1,31 +1,32 @@
 package BitManipulation;
 
 //find the two non repeating element in an array where
-// every element repeats itself
+// every element repeats itself twice
 public class xor_ques2 {
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 7, 9, 11, 2, 3, 11};
+        int[] arr = {2, 3, 7, 9, 11, 2, 3, 11};
         int n = arr.length;
-        twoNonRepeat(arr,n);
-
-
+        twoNonRepeat(arr, n);
     }
 
     static void twoNonRepeat(int arr[], int n) {
-        int temp = 0;
+        int temp = arr[0];
+        int right;
         for (int i = 0; i < n; i++) {
             temp = (temp ^ arr[i]);
+
         }
-        temp = (temp & -temp);
+        right = (temp & -temp);
         int temp1 = 0;
         int temp2 = 0;
-        for (int i = 0; i < n; i++) {
-            if ((arr[i] & temp) > 0) {
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] & right) != 0) {
                 temp1 = (temp1 ^ arr[i]);
             } else {
                 temp2 = (temp2 ^ arr[i]);
             }
         }
+
         System.out.println("the non repeating elements are : "
                 + temp1 + " and " + temp2);
 
