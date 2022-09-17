575342d9df12544479898e43c1c30bfa67ce7dc0
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-09-17 16:18:56 +0530

    init

--- src/Recursion/sum_of_n.java
+++ src/Recursion/sum_of_n.java
@@ -4,10 +4,12 @@ public class sum_of_n {
     public static void main(String[] args) {
         System.out.println(sum(5));
     }
-   static int sum(int n){
-        if (n==1)
+
+    static int sum(int n) {
+        if (n == 1)
             return 1;
-        return n+sum(n-1);
+        int partialans = sum(n - 1);
+        return n + partialans;
 
     }
 }
