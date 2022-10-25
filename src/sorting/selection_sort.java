3e7b2feafdfc92871d8deebe1f20b5ceacef8d89
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-10-25 17:02:47 +0530

    init

--- src/sorting/selection_sort.java
+++ src/sorting/selection_sort.java
@@ -4,24 +4,25 @@ import static sorting.selection.selectionSort;
 
 public class selection_sort {
     public static void main(String[] args) {
-        int a[] = {10,5,8,20,2,18};
-        selectionSort(a, 4);
+        int a[] = {10, 5, 8, 20, 2, 18};
+        selectionSort(a, 6);
 
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 6; i++) {
             System.out.print(a[i] + " ");
         }
 
     }
 }
-class selection{
+
+class selection {
 
 
-    static void selectionSort(int arr[], int n){
-        for(int i = 0; i < n; i++){
+    static void selectionSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
             int min_ind = i;
 
-            for(int j = i + 1; j < n; j++){
-                if(arr[j] < arr[min_ind]){
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_ind]) {
                     min_ind = j;
                 }
             }
