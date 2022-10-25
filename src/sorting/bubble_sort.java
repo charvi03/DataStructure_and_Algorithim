8c5d5b2acd261e8bca1de74661804ef35ebc7852
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-10-25 16:34:34 +0530

    init

---
+++ src/sorting/bubble_sort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+import static sorting.bubble.bubbleSort;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        int a[] = {2, 1, 4, 3};
+        bubbleSort(a, 4);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+    }
+}
+
+class bubble {
+
+    static void bubbleSort(int arr[], int n) {
+        boolean swapped;
+
+        for (int i = 0; i < n; i++) {
+
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+    }
+}
