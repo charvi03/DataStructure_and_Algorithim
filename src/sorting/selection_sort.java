4ee67bc40b2eee8a1fff1bf16f92823759881c24
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-10-25 16:46:10 +0530

    init

---
+++ src/sorting/selection_sort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import static sorting.selection.selectionSort;
+
+public class selection_sort {
+    public static void main(String[] args) {
+        int a[] = {2, 1, 4, 3};
+        selectionSort(a, 4);
+
+        for(int i = 0; i < 4; i++){
+            System.out.print(a[i] + " ");
+        }
+
+    }
+}
+class selection{
+
+
+    static void selectionSort(int arr[], int n){
+        for(int i = 0; i < n; i++){
+            int min_ind = i;
+
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] < arr[min_ind]){
+                    min_ind = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min_ind];
+            arr[min_ind] = temp;
+        }
+    }
+}
