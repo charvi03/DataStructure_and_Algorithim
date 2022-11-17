86df3a2aacf020c4b33c3c6208f16576a344bd56
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-17 21:26:52 +0530

    INIT

---
+++ src/sorting/quick_sort.java
@@ -0,0 +1,45 @@
+package sorting;
+
+import static sorting.merging_sort.mergeSort;
+
+public class quick_sort {
+    public static void main(String[] args) {
+        int a[] = {8,4,7,9,3,10,5};
+//		quickSort(a, 0, a.length-1);
+
+        mergeSort(a, 0, a.length-1);
+
+        for(int e: a) System.out.print(e+" ");
+
+
+        }
+    static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    static int partition(int a[], int l, int h) {
+        int i = l;
+        int j = h;
+
+        int pivot = a[l];
+        while(i < j) {
+            while(i < j && a[i] <= pivot) i++;
+            while(a[j] > pivot) j--;
+            if(i < j) {
+                swap(a, i, j);
+            }
+        }
+        swap(a, l, j);
+        return j;
+    }
+
+    static void quickSort(int a[], int l, int h) {
+        if(l < h) {
+            int pivot = partition(a, l, h);
+            quickSort(a, l, pivot-1);
+            quickSort(a, pivot+1, h);
+        }
+    }
+    }
+
