0aebfad58e9a58a753f99a9aa3a7d2c22f7b8aa3
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-12-12 15:40:43 +0530

    INIT

---
+++ src/Trees/binary_tree.java
@@ -0,0 +1,52 @@
+package Trees;
+
+public class binary_tree {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+        inorder(root);
+        System.out.println();
+        preorder(root);
+        System.out.println();
+        postorder(root);
+    }
+
+    public static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public static void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.key + " ");
+        }
+    }
+}
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    Node(int k) {
+        key = k;
+        left = right = null;
+    }
+}
+
