ca75cb824f92f3ea190b044d66cf142e3f6bcb31
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-10 15:31:36 +0530

    INIT

---
+++ src/graphs/adjacencyList.java
@@ -0,0 +1,36 @@
+package graphs;
+
+import java.util.ArrayList;
+
+public class adjacencyList {
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<Integer>());
+
+        // Adding edges one by one
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+
+        printGraph(adj);
+    }
+
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+        for (ArrayList<Integer> integers : adj) {
+            for (Integer integer : integers) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
