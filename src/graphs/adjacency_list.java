e62f4d6ed908973349345a5a8a11ffa5f8498c22
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-28 16:33:15 +0530

    INIT

--- src/graphs/adjacency_list.java
+++ src/graphs/adjacency_list.java
@@ -1,6 +1,8 @@
 package graphs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class adjacency_list {
     static class edge {
@@ -17,28 +19,58 @@ public class adjacency_list {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<edge>();
         }
+        graph[0].add(new edge(0, 1));
         graph[0].add(new edge(0, 2));
 
-        graph[1].add(new edge(1, 2));
+        graph[1].add(new edge(1, 0));
         graph[1].add(new edge(1, 3));
 
         graph[2].add(new edge(2, 0));
-        graph[2].add(new edge(2, 1));
-        graph[2].add(new edge(2, 3));
+        graph[2].add(new edge(2, 4));
 
         graph[3].add(new edge(3, 1));
-        graph[3].add(new edge(3, 2));
+        graph[3].add(new edge(3, 4));
+        graph[3].add(new edge(3, 5));
 
+        graph[3].add(new edge(4, 2));
+        graph[3].add(new edge(4, 3));
+        graph[3].add(new edge(4, 5));
+
+        graph[3].add(new edge(5, 3));
+        graph[3].add(new edge(5, 4));
+        graph[3].add(new edge(5, 6));
+
+        graph[3].add(new edge(6, 5));
+
+    }
+
+    public static void bfs(ArrayList<edge> graph[], int v) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[v];
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (vis[curr] == false) {
+                System.out.println(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int v = 4;
+        int v = 7;
         ArrayList<edge> graph[] = new ArrayList[v];
         createGraph(graph);
 //print 2 neighbours
-        for (int i = 0; i < graph[2].size(); i++) {
-            edge e = graph[2].get(i);
-            System.out.println(e.dest + " ");
-        }
+//        for (int i = 0; i < graph[2].size(); i++) {
+//            edge e = graph[2].get(i);
+//            System.out.println(e.dest + " ");
+        bfs(graph, v);
+        System.out.println();
     }
 }
+
